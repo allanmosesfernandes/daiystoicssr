0de2ea916cb0e2c06c61bc81867bd61c6f2790ca
allanmosesfernandes <fernzallan14@gmail.com>
2025-07-05 15:58:50 +0100

    bookmarks

--- src/app/components/share.tsx
+++ src/app/components/share.tsx
@@ -43,51 +43,58 @@ const Share = ({ data }: { data: ShareData }) => {
 
     return (
         <>
-        <div className="dropdown dropdown-top mt-8 font-xl">
-            <div tabIndex={0} role="button" className="btn m-1 md:text-4xl text-2xl font-mf-consent min-w-[200px] py-6">Share stoic reminder</div>
-            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm font-2xl">
-                <li className=''>
-                    <a href="">
-                        <TwitterShareButton
-                            url={shareURL}
-                            title={tweetText}
-                        >
-                            Twitter
-                        </TwitterShareButton>
-                    </a>
-                </li>
-                <li className=''>
-                    <a href="">
-                        <LinkedinShareButton
-                            url={shareURL}
-                            title={tweetText}
-                        >
-                            Linkedin
-                        </LinkedinShareButton>
-                    </a>
-                </li>
-                <li className=''>
-                    <a href="">
-                        <FacebookShareButton
-                            url={shareURL}
-                            quote={tweetText}
-                        >
-                            Facebook
-                        </FacebookShareButton>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                        <WhatsappShareButton
-                            url={shareURL}
-                            title={tweetText}
-                            separator="\n\n"
-                            >Whatsapp
-                        </WhatsappShareButton>
-                    </a>
-                </li>
-            </ul>
-        </div>
+            <div className="dropdown dropdown-top mt-8 font-xl">
+                <div tabIndex={0} role="button" className="btn btn-neutral group m-1 text-2xl font-mf-consent min-w-[150px] py-6 relative">
+                    <span className="transition-all group-hover:opacity-0 group-hover:scale-90">Share</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="absolute w-8 h-8 opacity-0 transition-all group-hover:opacity-100 group-hover:scale-100"><path fill="currentColor" fillRule="evenodd" d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z"></path></svg>
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm font-2xl">
+                    <li className=''>
+                        <a href="">
+                            <FacebookShareButton
+                                url={shareURL}
+                                quote={tweetText}
+                            >
+                                Facebook
+                            </FacebookShareButton>
+                        </a>
+                    </li>
+                    <li className=''>
+                        <a href="">
+                            <LinkedinShareButton
+                                url={shareURL}
+                                title={tweetText}
+                            >
+                                Linkedin
+                            </LinkedinShareButton>
+                        </a>
+                    </li>
+                    <li className=''>
+                        <a href="">
+                            <TwitterShareButton
+                                url={shareURL}
+                                title={tweetText}
+                            >
+                                Twitter
+                            </TwitterShareButton>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <WhatsappShareButton
+                                url={shareURL}
+                                title={tweetText}
+                                separator="\n\n"
+                                >Whatsapp
+                            </WhatsappShareButton>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <button className="btn m-1 text-2xl font-mf-consent min-w-[150px] py-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+            </svg>Bookmark quote</button>
         </>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,17 +24,17 @@ export default function Home() {
     <main className="min-h-screen flex mt-4 justify-center p-8">
       <div className="max-w-5xl mx-auto text-center">
         {/* Header */}
-        <h1 className="text-4xl md:text-8xl font-mf-consent mb-6 border-b-2 border-base-content text-white">Reminder of the day</h1>
+        <h1 className="text-4xl md:text-8xl font-mf-consent mb-6 border-b-2 border-white text-base-content">Reminder of the day</h1>
         <p className="md:text-lg text-sm text-white mb-4">{formattedDate}</p>
-        <h1 className="md:text-3xl text-lg mb-6 text-white">{quoteData.title}</h1>
+        <h1 className="md:text-3xl text-lg mb-6 text-base-content">{quoteData.title}</h1>
         <p className="md:text-lg text-sm text-white mb-6">
           {quoteData.quote}
         </p>
-        <p className="md:text-sm text-xs text-white mb-6 italic">
+        <p className="md:text-sm text-xs text-base-content mb-6 italic">
           - {quoteData.author}
         </p>
         <p className="md:text-2xl text-sm text-white">
-          <span className="font-mf-consent text-6xl items-center">T</span>he goodness inside you is like a small flame, and you are its keeper. It's your job, today and every day, to make sure that it has enough fuel, that it doesn't get obstructed or snuffed out. Every person has their own version of the flame and is responsible for it, just as you are. If they all fail, the world will be much darker—that is something you don't control. But so long as your flame flickers, there will be some light in the world.
+          <span className="font-mf-consent text-6xl items-center text-base-content">T</span>he goodness inside you is like a small flame, and you are its keeper. It's your job, today and every day, to make sure that it has enough fuel, that it doesn't get obstructed or snuffed out. Every person has their own version of the flame and is responsible for it, just as you are. If they all fail, the world will be much darker—that is something you don't control. But so long as your flame flickers, there will be some light in the world.
         </p>
         {/* Share Component */}
         <Share data={quoteData}/>
