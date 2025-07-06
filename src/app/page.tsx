fdd2486c728c08023a753caa5e7f96dd594d8b9d
allanmosesfernandes <fernzallan14@gmail.com>
2025-07-06 14:24:47 +0100

    add-new-color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,22 +50,22 @@ export default function Home() {
 
   return (
     <main className="flex mt-4 justify-center p-4">
-      <div className="max-w-5xl mx-auto text-center">
+      <div className="max-w-5xl mx-auto text-center color-base">
         {/* Header */}
-        <h1 className="text-4xl md:text-6xl font-mf-consent pb-2 border-b border-white text-base-content">Reminder of the day</h1>
-        <p className="md:text-lg text-sm text-white my-4">{formattedDate}</p>
-        {loading && <p className="text-white">Loading today&apos;s quote...</p>}
+        <h1 className="text-4xl md:text-6xl font-mf-consent pb-2 border-b text-base-content">Reminder of the day</h1>
+        <p className="md:text-lg text-sm text-[var(--color-offwhite)] my-4">{formattedDate}</p>
+        {loading && <p className="text-white color-base">Loading today&apos;s quote...</p>}
         {error && <p className="text-red-500">Error: {error}</p>}
         {quoteData && (
           <>
             <h1 className="md:text-2xl font-bold text-sm text-base-content">{quoteData.title}</h1>
-            <p className="md:text-lg text-sm text-white my-4">
+            <p className="md:text-lg text-sm text-[var(--color-offwhite)] my-4">
               {quoteData.quote}
             </p>
-            <p className="text-sm font-bold text-base-content italic">
+            <p className="text-sm font-bold text-base-content italic color-base">
               - {quoteData.author}
             </p>
-            <p className="md:text-lg text-sm text-white my-4">
+            <p className="md:text-lg text-sm my-4 text-[var(--color-offwhite)]">
               <span className="font-mf-consent text-6xl items-center text-base-content">T</span>{quoteData.text.substring(1)}
             </p>
             {/* Share Component */}
